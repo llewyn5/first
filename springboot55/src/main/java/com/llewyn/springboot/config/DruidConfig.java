121e29527f7ae2de97f332c39e417cde8c836124
llewyn <llewyn5@outlook.com>
2018-11-20 23:42:36 +0800

    数据源

---
+++ springboot55/src/main/java/com/llewyn/springboot/config/DruidConfig.java
@@ -0,0 +1,24 @@
+package com.llewyn.springboot.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DruidConfig {
+
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DataSource druid(){
+        return new DruidDataSource();
+    }
+    //配置druid的监控
+    //1.配置一个管理后台的servlet
+
+    //2.配置一个监控的filter
+}
---
+++ springboot55/src/main/java/com/llewyn/springboot/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.llewyn.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class HelloController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    public Map<String,Object> map(){
+        List<Map<String,Object>> list = jdbcTemplate.queryForList("select * from department");
+        return list.get(0);
+    }
+}
