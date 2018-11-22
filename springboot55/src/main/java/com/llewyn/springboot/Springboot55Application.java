7a183d99473565147dee6e807789f56e01da8f75
llewyn <llewyn5@outlook.com>
2018-11-22 10:40:42 +0800

    all

---
+++ springboot55/src/main/java/com/llewyn/springboot/Springboot55Application.java
@@ -0,0 +1,12 @@
+package com.llewyn.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot55Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot55Application.class, args);
+    }
+}
